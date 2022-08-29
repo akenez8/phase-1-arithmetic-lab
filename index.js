120d92b533af333da04a6e12fd82b57b3d9eb050
peter <+>
2022-08-29 13:57:07 -0400

    commit tree 1395514fd9a05f6bffb8322f1b2e97fdb6f1bbf3

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 16;
+let num4 = 4;
+let product;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random());
+let mod = Math.ceil(num3 / num4);
+let max = Math.max(2,4,8,16,20);
